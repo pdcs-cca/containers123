1841c39bb75de0e50ec967463c6c4e14a0ec1793
Pedro Damián Cruz Santiago <55673421+pdcs-cca@users.noreply.github.com>
2025-11-13 14:04:43 -0600

    Enhance copiar-raiz.sh with usage info and checks
    Added usage instructions and improved argument handling.

--- copiar-raiz.sh
+++ copiar-raiz.sh
@@ -1,5 +1,22 @@
 #!bin/bash
-_APP=${1:-/bin/bash}
+
+_uso(){
+echo "
+Uso bash ./$0 APP DIR_RAIZ:
+
+Ejemplo
+./$0 /bin/ps nroot"
+
+exit 123
+}
+
+test $# -lt 2 && _uso
+
+_APP=$1
+_RAIZ=$2
+test ! -x $_APP && _uso
+
+_LD_LINUX="/lib64/ld-linux-x86-64.so.2 /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2"
 _LIBS=$(ldd $_APP | awk ' NF > 2 {system("echo "$3"*")}' | xargs)
-_RAIZ=${2:-raiz}
-rsync -PRa $_APP $_LIBS $_RAIZ
+
+rsync -PRa -v $_APP $_LD_LINUX $_LIBS $_RAIZ
